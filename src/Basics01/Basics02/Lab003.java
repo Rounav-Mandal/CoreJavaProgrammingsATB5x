c6427186a18a15a62b6ea10a14f9012c1f6874a3
Rounav-Mandal <rounav.job@gmail.com>
2024-01-04 18:53:04 +0530

    I am pushing Lab002.java and Lab003.java to GitHub

---
+++ src/Basics01/Basics02/Lab003.java
@@ -0,0 +1,9 @@
+package src.Basics01.Basics02;
+
+public class Lab003 {
+
+    public static void main(String[] args) {
+
+        System.out.println("First Project");
+    }
+}
---
+++ src/Basics01/Lab002.java
@@ -0,0 +1,9 @@
+package src.Basics01;
+
+public class Lab002 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Syncing with GitHub");
+    }
+}
